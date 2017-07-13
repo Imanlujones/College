bc6befe218d76fcb23f77e592948292a88d6e12a
christianju <christianjudkins@gmail.com>
2017-07-13 09:51:16 -0400

    Second change
    made new html for the third handler

--- handlers/third_handler.py
+++ handlers/third_handler.py
@@ -5,3 +5,7 @@ import webapp2
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
+
+
+  template = jinja_env.env.get_template('templates/profile.html')
+  self.response.out.write(template.render(html_params))
