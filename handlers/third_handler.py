a8d754bef6a20908d343e27610a8af1f69b9a120
christianju <christianjudkins@gmail.com>
2017-07-13 10:12:26 -0400

    Change with error

--- handlers/third_handler.py
+++ handlers/third_handler.py
@@ -6,6 +6,12 @@ import webapp2
 from google.appengine.ext import ndb
 from google.appengine.api import users
 
-
-  template = jinja_env.env.get_template('templates/profile.html')
-  self.response.out.write(template.render(html_params))
+class ThirdHandler(webapp2.RequestHandler):
+	def get(self):
+		logging.info("MainHandler")
+        html_params = {
+            "title": "Profile",
+            "content": "Hello"
+        }
+        template = jinja_env.env.get_template('/templates/profile.html')
+        self.response.out.write(template.render(html_params))
