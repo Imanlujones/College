313b3e5eb0f4f61692169fc932195cf12586a6a4
christianju <christianjudkins@gmail.com>
2017-07-14 09:06:45 -0400

    Adding css and added headers

--- handlers/third_handler.py
+++ handlers/third_handler.py
@@ -6,12 +6,13 @@ import webapp2
 from google.appengine.ext import ndb
 from google.appengine.api import users
 
+
 class ThirdHandler(webapp2.RequestHandler):
-	def get(self):
-		logging.info("MainHandler")
+    def get(self):
+        template = jinja_env.env.get_template('/templates/profile.html')
+        logging.info("MainHandler")
         html_params = {
             "title": "Profile",
             "content": "Hello"
         }
-        template = jinja_env.env.get_template('/templates/profile.html')
         self.response.out.write(template.render(html_params))
