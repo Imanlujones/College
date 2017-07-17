0e217c99c67711b0a3ab193ce3bddc5996073c97
christianju <christianjudkins@gmail.com>
2017-07-17 00:43:59 -0400

    Updates to the profile page, created handler # 4

---
+++ College/handlers/fourth_handler.py
@@ -0,0 +1,7 @@
+
+import jinja_env
+import logging
+import webapp2
+
+from google.appengine.ext import ndb
+from google.appengine.api import users
---
+++ College/handlers/main_handler.py
@@ -0,0 +1,14 @@
+
+import jinja_env
+import logging
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+    	logging.info("MainHandler")
+        html_params = {
+            "title": "College Ebay",
+            "content": "Hello"
+        }
+        template = jinja_env.env.get_template('templates/maintmpl.html')
+        self.response.out.write(template.render(html_params))
