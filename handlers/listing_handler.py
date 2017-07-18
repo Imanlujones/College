0d1723164dbbd943cfe31924760b802a47c26b7e
Imanlujones <imanlujones@gmail.com>
2017-07-18 18:38:42 -0400

    changes to html
    query params

--- handlers/listing_handler.py
+++ handlers/listing_handler.py
@@ -2,13 +2,14 @@
 import jinja_env
 import logging
 import webapp2
+from models import sales_model
 
 from models import book
 
 class listingHandler(webapp2.RequestHandler):
     def get(self):
         logging.info("listingHandler")
-        books = book.Book.query().fetch()
+        new_items = sales_model.SalesModel.query(sales_model.SalesModel.category_name==self.request.get("type")).fetch()
         # do stuff with books...
         html_params = {
             "title": "Second Title",
