7734590b71ab3f7d2460b06c2d5ab2f89461f71b
christianju <christianjudkins@gmail.com>
2017-07-18 08:55:16 -0400

    Got post to get remembered in logs

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from handlers import main_handler
 from handlers import second_handler
 from handlers import login_handler
 from handlers import third_handler
-from handlers import fourth_handler
+from handlers import sales_handler
 
 jinja_env.init(os.path.dirname(__file__))
 
@@ -14,5 +14,5 @@ app = webapp2.WSGIApplication([
     ('/', main_handler.MainHandler),
     ('/second', second_handler.SecondHandler),
     ('/profile', third_handler.ThirdHandler), 
-    ('/sales', sales_handler.FourthHandler),
+    ('/sales', sales_handler.SalesHandler),
 ], debug=True)
